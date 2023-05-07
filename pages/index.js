0af63467ef2e645bd3862217aababd66cc7eac5f
Eric100k <ericpatricwest@hotmail.com>
2023-05-06 21:28:53 -0300

    teste de link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
+
+
 function home() {
-    return <div > HomeXXXsdada < /div>
+    return <dv><h1> Home </h1>
+
+    <link href='./sobre'>
+        <a>Acessar pagina sobre</a>
+    </link>
+    </dv>
 }
 
 export default home
---
+++ pages/sobre.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+function sobre() {
+    return <dv><h1> Sobre </h1>
+
+    <link href='./sobre'>
+        <a>Acessar pagina Home</a>
+    </link>
+    </dv>
+}
+
+export default sobre
