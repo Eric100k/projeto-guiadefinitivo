4879148e410fab62880d14308220f9d8e9427dc9
Eric100k <ericpatricwest@hotmail.com>
2023-05-06 23:53:24 -0300

    teste de link 02

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Link from 'next/link';
 
-function home() {
-    return <div><h1> Home </h1>
 
-    <link href='/sobre'>
-        <a>Acessar pagina sobre</a>
-    </link>
-    </div>
+function home() {
+    return (<div>
+        <h1> Home </h1>
+
+        <Link href="/sobre">
+            <a>Acessar pagina sobre</a>
+        </Link>
+
+    </div>)
 }
 
 export default home
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';   
+
+function sobre() {
+    return (<div>
+        <h1> Sobre </h1>
+
+        <Link href="/">
+            <a>Acessar pagina Home</a>
+        </Link>
+
+    </div>)
+}
+
+export default sobre
