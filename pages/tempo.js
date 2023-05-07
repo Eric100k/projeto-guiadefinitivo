f0a209df1f17ca8e9459a2d82d34537e4cf9b1a0
Eric100k <ericpatricwest@hotmail.com>
2023-05-07 00:45:29 -0300

    teste static

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,5 +1,5 @@
 function Tempo(props) {
-
+    console.log('>Passando pelo FrontEnd();')
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
 
@@ -13,7 +13,7 @@ function Tempo(props) {
 }
 
 export function getStaticProps() {
-
+    console.log('>Passando pelo getStaticProps();')
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
 
