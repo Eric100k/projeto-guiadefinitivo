029fe46c4e3c5fc279f88c2f4ab1383bb849bbbe
Eric100k <ericpatricwest@hotmail.com>
2023-05-06 22:52:49 -0300

    eita

--- pages/sobre.js
+++ pages/sobre.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 function sobre() {
     return <dv><h1> Sobre </h1>
 
-    <link href='./sobre'>
+    <link href='/sobre'>
         <a>Acessar pagina Home</a>
     </link>
     </dv>
